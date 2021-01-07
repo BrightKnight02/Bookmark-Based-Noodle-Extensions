8b3379b6c4c1e3178d964141010ef7a80231c128
BrightKnight <kyled2002@hotmail.com>
2021-01-07 18:27:07 -0500

    uhhh adding notes seems to work

--- bookmark parse test.py
+++ bookmark parse test.py
@@ -1,6 +1,7 @@
 import json
 import sys
 import os
+import math as m
 
 #https://stackoverflow.com/questions/1176136/convert-string-to-python-class-object
 #This takes a str and convert to an object
@@ -19,14 +20,14 @@ def getFile():
             print("Error: File does not exist in this location.")
     return fileName
     
-def getEffectData(jsonObject):
+def getEffectData():
     effectMarks = []
-    for x in jsonObject['_customData']['_bookmarks']:
+    for x in diff['_customData']['_bookmarks']:
         if x['_name'][0] == "/":
             effectMark = []
             temp = x['_name'].split()
             effectMark.append(strToClass(temp[1]))
-            data = [x["_time"]]
+            data = [x['_time']]
             for i in range(2, len(temp)):
                 data.append(temp[i])
             effectMark.append(data)
@@ -36,13 +37,16 @@ def getEffectData(jsonObject):
     
 
 def main():
-    #fileName = getFile()
-    fileName = "HardStandard.dat" #temp for testing
-    file = open(fileName, "r")
-    jsonObject = json.load(file)
-    for x in getEffectData(jsonObject):
-        x[0]()
-    file.close()
+    global diff
+    #diffFileName = getFile()
+    diffFileName = "HardStandard.dat" #temp for testing
+    diffFile = open(diffFileName, "r")
+    diff = json.load(diffFile)
+    diffFile.close()
+    for x in getEffectData():
+        x[0](x[1])
+    diffFile = open(diffFileName, "w")
+    json.dump(diff, diffFile)
     
     
     
@@ -52,14 +56,29 @@ def main():
 #  must be after the / in your bookmark for the effect to be applied
 #-----------------------------------------------------------------------------
 
-def test1():
-    print("1")
+def test1(data):
+    print(data)
     
-def test2():
-    print("2")
+def test2(data):
+    print(data)
     
-def test3():
-    print("3")
+def test3(data):
+    print(data)
+    
+def addNoteOnBeats(data):
+    start = data[0]
+    stop = float(data[1])
+    for time in range(m.ceil(start), int(m.floor(stop) + start + 1)):
+        note = {"_time": time, 
+                "_lineIndex": 1, 
+                "_lineLayer": 0, 
+                "_type": 0, 
+                "_cutDirection": 1,}
+        if note not in diff['_notes']:
+            diff['_notes'].append(note)
+    
+    
+    
     
 
 # End of effects
