0b030ae81b05fdb3c2ccfee1ec92ec11da2745a5
BrightKnight <kyled2002@hotmail.com>
2021-01-07 12:56:17 -0500

    Create bookmark parse test.py

---
+++ bookmark parse test.py
@@ -0,0 +1,13 @@
+import json
+import sys
+
+#https://stackoverflow.com/questions/1176136/convert-string-to-python-class-object
+#This takes a str and convert to an object
+#have to set a variable equal to it then run the variable
+def strToClass(classname):
+    return getattr(sys.modules[__name__], classname)
+
+def getFile()
+
+if __name__ = "__main__":
+    getFile()
